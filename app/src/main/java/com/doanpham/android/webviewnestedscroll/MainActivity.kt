dc8a860d7365040f832c461249da3b2c952f8c42
DoanPham <doannphamm57@gmail.com>
2025-09-12 07:19:35 +0700

    task: remove cloned Scroll

--- app/src/main/java/com/doanpham/android/webviewnestedscroll/MainActivity.kt
+++ app/src/main/java/com/doanpham/android/webviewnestedscroll/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.doanpham.android.webviewnestedscroll
 
-import android.content.Context
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
@@ -12,52 +11,14 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.doanpham.android.webviewnestedscroll.ui.theme.WebViewNestedScrollTheme
-import rememberScrollState
-import verticalScroll
-
-class NoScrollWebView(
-    context: Context,
-) : WebView(context) {
-
-    override fun scrollTo(x: Int, y: Int) {
-        super.scrollTo(x, y)
-        println(">>>David - NoScrollWebView scrollTo: x: $x, y: $y")
-
-    }
-
-    override fun computeScroll() {
-//        super.computeScroll()
-        println(">>>David - NoScrollWebView computeScroll")
-
-    }
-
-    override fun scrollBy(x: Int, y: Int) {
-        super.scrollBy(x, y)
-        println(">>>David - NoScrollWebView scrollBy: x: $x, y: $y")
-
-    }
-
-    override fun onScrollChanged(l: Int, t: Int, oldl: Int, oldt: Int) {
-        super.onScrollChanged(l, t, oldl, oldt)
-        println(">>>David - NoScrollWebView onScrollChanged: t: $t, oldt: $oldt")
-
-        // Force scroll back to (0, 0) to prevent internal scrolling
-//        if (t != 0 || l != 0) {
-//            scrollTo(0, 0)
-//        }
-    }
-}
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalLayoutApi::class)
@@ -107,12 +68,10 @@ class MainActivity : ComponentActivity() {
 
             Column {
                 Spacer(Modifier.height(80.dp))
+
                 Column(
                     modifier = Modifier
                         .border(3.dp, Color.Red)
-                        .onSizeChanged {
-                            println("7453 - Column - size: $it")
-                        }
                         .verticalScroll(scrollState)
                         .fillMaxSize()
                 ) {
@@ -124,12 +83,7 @@ class MainActivity : ComponentActivity() {
                             .border(2.dp, Color.Blue)
                             .fillMaxSize(),
                         factory = { context ->
-                            NoScrollWebView(context).apply {
-//                                settings.builtInZoomControls = false  // Disables pinch/zoom gestures
-//                                settings.displayZoomControls = false  // Hides zoom buttons
-//                                settings.setSupportZoom(false)
-
-
+                            WebView(context).apply {
                                 loadDataWithBaseURL(
                                     null,
                                     html8000,
@@ -141,36 +95,9 @@ class MainActivity : ComponentActivity() {
                         }
                     )
                 }
+
                 Spacer(Modifier.height(80.dp))
             }
-
-
-
-            LaunchedEffect(Unit) {
-                snapshotFlow { scrollState.value }.collect { println("7453 - scrollState value: $it") }
-            }
-            LaunchedEffect(scrollState.maxValue) {
-                println("7453 - scrollState maxValue: ${scrollState.maxValue}")
-            }
-            LaunchedEffect(scrollState.viewportSize) {
-                println("7453 - scrollState viewportSize: ${scrollState.viewportSize}")
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WebViewNestedScrollTheme {
-        Greeting("Android")
     }
 }
